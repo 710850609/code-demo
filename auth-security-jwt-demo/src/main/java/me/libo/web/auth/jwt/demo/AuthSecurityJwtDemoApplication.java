8bcc0225c6c061a1fab5a2e2f2aa9022feb481f5
710850609@qq.com <5318099www>
2020-01-26 20:52:42 +0800

    完善jwt认证模块

---
+++ auth-security-jwt-demo/src/main/java/me/libo/web/auth/jwt/demo/AuthSecurityJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package me.libo.web.auth.jwt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSecurityJwtDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSecurityJwtDemoApplication.class, args);
+    }
+
+}
